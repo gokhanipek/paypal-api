cc95e39a140c57fdd1bbc6bf8714d87c0775ce6c
ipekg <goekhan.ipek@tipico.com>
2024-02-14 17:04:17 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,49 +1,70 @@
 /* global paypal */
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import "./app.css";
+
+export const amountValues = [5, 10, 20, 50, 100];
 
 export default function App() {
   const paypalRef = useRef();
+  const [amount, setAmount] = useState(5);
 
-  useEffect(()=>{
-    paypalRef.current = paypal.Buttons();
-    paypalRef.current.render('#paypal-button-container');
-  }, [])
-
-  const onClickHandler = () => {
-    paypalRef.current.getState().then(data => {
-      fetch("https://tuz.at/lab/gentoken/classic/setec_ba/")
-        .then(function(response) {
+  useEffect(() => {
+    const button = paypal.Buttons({
+      onClick: (e) => {
+        fetch("https://tuz.at/lab/gentoken/classic/setec_ba/")
+          .then(function (response) {
             return response.text();
-        })
-        .then(function(token) {
-            var button = paypal.Buttons({
-                createOrder: function(data, actions) {
-                    console.log(data);
-                    return token;
-                },
-                onApprove: function(data, actions) {
-                    alert('success');
-                },
-                onCancel: function(data, actions) {
-                  alert('error');
-                }
-            });
-            if (button.isEligible()) {
-                button.render('#paypal-button-container');
-            }
-        });
-    })
+          })
+          .then({
+            createOrder: function (data, actions) {
+              console.log(amount);
+              return data;
+            },
+            onApprove: function (data, actions) {
+              alert()
+            },
+            onCancel: function (data, actions) {
+              //redirect to which url was provided in the response
+            },
+          });
+      },
+    });
+    button.render("#paypal-button-container");
+  }, [amount]);
+
+  const onClickHandler = (val) => {
+    setAmount(val);
   };
+
+  // paypalRef.current.getState().then((data) => {
+  //   fetch("https://tuz.at/lab/gentoken/classic/setec_ba/")
+  //     .then(function (response) {
+  //       return response.text();
+  //     })
+  //     .then(function (token) {
+  //       var button = paypal.Buttons({
+  //
+  //       if (button.isEligible()) {
+  //         button.render("#paypal-button-container");
+  //       }
+  //     });
+  // });
 
   return (
     <>
-      <div
-        onClick={onClickHandler}
-        id="paypal-button-container"
-      >
-        Paypal
-      </div>
+      <form onChange={onClickHandler}>
+        {amountValues.map((amount) => {
+          return (
+            <>
+              <label>{amount}</label>
+              <input value={amount} type="radio" />
+            </>
+          );
+        })}
+        <div ref={paypalRef} id="paypal-button-container">
+          Paypal
+        </div>
+      </form>
     </>
   );
 }
